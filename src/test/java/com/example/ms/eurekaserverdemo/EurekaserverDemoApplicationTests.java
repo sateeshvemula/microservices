e502e13da62451a55082f9d8b30baf455b8e69fb
Sateesh Vemula <svemula@nisum.com>
2019-12-13 18:16:02 +0530

    first commit

---
+++ src/test/java/com/example/ms/eurekaserverdemo/EurekaserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
